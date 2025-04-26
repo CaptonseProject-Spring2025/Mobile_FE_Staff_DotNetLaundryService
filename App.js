5075de2f9aff4bf6f6aaced7fafc7e8c6cca73ba
ThanhB <bintri92@gmail.com>
2025-04-26 21:15:07 +0700

    fix noti

--- App.js
+++ App.js
@@ -10,25 +10,50 @@ import { enableScreens } from "react-native-screens";
 import MapboxGL from "@rnmapbox/maps";
 import Toast from "react-native-toast-message";
 import "./global.css";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as Notifications from "expo-notifications";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+
 const tokenAccess =
   "pk.eyJ1IjoidGhhbmhidCIsImEiOiJjbThrY3U3cm4wOWliMm5zY2YxZHphcGhxIn0.XFTGLomzaK65jyUYJCLUZw";
 
 MapboxGL.setAccessToken(tokenAccess);
 
+// Configure notification handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
 enableScreens();
+
+function AppContents() {
+  // This component will have access to navigation
+  const Navigation = require("./api/store/usePushNotifications").default;
+  return (
+    <>
+      <KeyboardProvider>
+        <Layout />
+        <Navigation />
+      </KeyboardProvider>
+    </>
+  );
+}
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}> 
-    <SafeAreaProvider>
-      <PaperProvider>
-        <NavigationContainer>
-          <Layout />
-        </NavigationContainer>
-      </PaperProvider>
-      <Toast />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <PaperProvider>
+          <NavigationContainer>
+            <AppContents />
+          </NavigationContainer>
+        </PaperProvider>
+        <Toast />
+      </SafeAreaProvider>
     </GestureHandlerRootView>
   );
 }
